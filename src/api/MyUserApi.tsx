21221e60a763ae290896f0d544a4413e30f88fd9
Vit Wits <vitwits@gmail.com>
2024-06-07 18:22:37 +0200

    added custom hook for updating user profile info

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -42,3 +42,44 @@ export const useCreateMyUser = () => {
     isSuccess,
   };
 };
+
+type updateMyUserRequest = {
+  name: string;
+  addressLine1: string;
+  city: string;
+  country: string;
+};
+
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyUserRequest = async (formData: updateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+  };
+
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+
+  return {
+    updateUser,
+    isLoading,
+  };
+};
